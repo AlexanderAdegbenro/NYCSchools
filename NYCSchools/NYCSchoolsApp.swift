23a989a66083edd712bd0cf5473588cb1a771451
Alexander Adegbenro <52863002+AlexanderAdegbenro@users.noreply.github.com>
2022-06-07 08:54:58 -0400

    Initial Commit

---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by Alexander Adgebenro on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
